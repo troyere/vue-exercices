1e1375bbea351e28e9d2d2b5f62a968fa34169f9
Tanguy ROYERE <t.royere@alptis.fr>
2023-04-04 18:29:50 +0200

    feat: question 2

---
+++ src/components/Question2/Question2Form.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const text = ref('');
+
+const onInput = (event: InputEvent): void => {
+  text.value = (event.target as HTMLInputElement).value;
+};
+</script>
+
+<template>
+  <form>
+    <input :model-value="text" @input="onInput" />
+
+    <p class="typed-text"><em>Vous avez écrit :</em> {{ text }}</p>
+  </form>
+</template>
--- src/views/Question2View.vue
+++ src/views/Question2View.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
 import DefaultMain from '../components/DefaultMain.vue';
+import Question2Form from '../components/Question2/Question2Form.vue';
 </script>
 
 <template>
-  <DefaultMain title="Question 2"></DefaultMain>
+  <DefaultMain>
+    <template #title>Question 2</template>
+
+    <template #subTitle>Comment simplifier le code du composant Question2Form.vue ?</template>
+
+    <template #default>
+      <Question2Form />
+    </template>
+  </DefaultMain>
 </template>
